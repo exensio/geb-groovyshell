d0f30a676ff0a84f5a018571a0d71645fb29fec1
flmu <florian.mutter@exensio.de>
2016-03-29 12:32:16 +0200

    Do not use fixed firefox path

--- src/main/groovy/startGeb.groovy
+++ src/main/groovy/startGeb.groovy
@@ -1,15 +1,8 @@
 import geb.Browser
-import org.openqa.selenium.firefox.FirefoxBinary
-import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.firefox.FirefoxProfile
-import org.openqa.selenium.firefox.internal.ProfilesIni
 import geb.binding.BindingUpdater
+import org.openqa.selenium.firefox.FirefoxDriver
 
-pathToFirefox = new File("C:\\Program Files (x86)\\Mozilla Firefox ESR 38\\firefox.exe")
-firefoxBinary = new FirefoxBinary(pathToFirefox)
-profilesIni = new ProfilesIni()
-firefoxProfile = profilesIni.getProfile("Firefox38ESRDEV")
-firefoxDriver = new FirefoxDriver(firefoxBinary, firefoxProfile)
+firefoxDriver = new FirefoxDriver()
 
 browser = new Browser(driver: firefoxDriver)
 

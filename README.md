140278780066c16c2a49e656ddee74e8ab666f99
Florian Mutter <florian.mutter@exensio.de>
2017-05-05 15:11:15 +0200

    Use chrome instead of firefox

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Interactive Geb shell
 
-Starts Firefox and provides a [groovy shell](http://www.groovy-lang.org/groovysh.html) to execute [geb](http://www.gebish.org) commands.
+Starts Chrome and provides a [groovy shell](http://www.groovy-lang.org/groovysh.html) to execute [geb](http://www.gebish.org) commands.
 
 When starting to learn geb I wished to have a simple environment to try out some code snippets
 before using them in my functional tests.
@@ -34,55 +34,17 @@ Inside groovy shell:
     browser.go("http://www.test.example") // protocol is needed
     def heading = browser.find(".heading") // finds the element with class 'heading'
     heading.text() // get the heading's text
-    
-### Shutting down
-
-Use the provided ```stopGeb.groovy``` to close the browser:
-
-Inside groovy shell:
-
-    :load src/main/groovy/stopGeb.groovy // loads commands from file
-    :exit // exit the groovy shell
 
 ## Config
 
-If you have multiple versions of firefox installed, you can specify which is used with the `webdriver.firefox.bin`
-system property:
+All system properties are passed to the shell.
 
+Example:
     ./gradlew geb-groovyshell -q -Dwebdriver.firefox.bin="/Applications/Firefox ESR.app/Contents/MacOS/firefox-bin"
-    
-Alternatively you can use ```gradle.properties``` to do the same.
-An example is already included but commented out.
-
-All system properties are passed to the shell. See https://github.com/SeleniumHQ/selenium/wiki/FirefoxDriver for
-additional webdriver settings.
-
-## Versioning
-
-* The ```master``` branch should always use the latest geb and webdriver versions.
-* Other branches will contain older versions. They may need also an older firefox version.
 
 ## Troubleshooting
 
-* After starting the browser you should be passed back to the groovy shell and be able to start typing your commands. If not: Most problems are caused by mismatching selenium and firefox version. You could try another combination by modifing the following lines in ```build.gradle```:
+* After starting the browser you should be passed back to the groovy shell and be able to start typing your commands. If not: Most problems are caused by mismatching selenium and chrome version. You could try another combination by modifing `gebVersion` and `webdriverVersion` in `build.gradle` and the chrome driver version in `startGeb.groovy`.
 
-```
-buildscript {
-    repositories {
-        mavenCentral()
-    }
-    dependencies {
-        classpath "org.gebish:geb-core:0.9.0" // <---
-        classpath "org.seleniumhq.selenium:selenium-support:2.46.0" // <---
-        classpath "org.seleniumhq.selenium:selenium-firefox-driver:2.46.0" // <---
-    }
-}
-```
 	
-* It is also possbile to use another browser vendor by modifing the line:
-
-```
-classpath "org.seleniumhq.selenium:selenium-firefox-driver:2.46.0"
-```
-
 See the offical selenium and geb docs for more information.
--- src/main/groovy/startGeb.groovy
+++ src/main/groovy/startGeb.groovy
@@ -1,16 +1,84 @@
-:set interpreterMode true
+import geb.Browser
+import geb.binding.BindingUpdater
+import org.openqa.selenium.Platform
+import org.openqa.selenium.chrome.ChromeDriver
 
-// Workaround: we can't call this directly:
-String script = "import geb.Browser\n" +
-        "import geb.binding.BindingUpdater\n" +
-        "import org.openqa.selenium.firefox.FirefoxDriver\n" +
-        "new BindingUpdater(new Binding(), new Browser(driver: new FirefoxDriver()))"
+import java.awt.*
 
-Binding sharedData = new Binding()
-GroovyShell shell = new GroovyShell(sharedData)
+chromeDriverVersion = "2.29"
 
-def updater = shell.evaluate(script).initialize()
-def browser = updater.browser
-def firefoxDriver = updater.browser.driver
+File downloadDriver(String currentPlatformName,
+        String driverDownloadFullPath,
+        String driverFilePath,
+        String archiveFileExtension,
+        String name,
+        String version) {
+    // to avoid always a fresh download in jenkins we use another directory
+    def subPath = "../../tools/"
+    File destinationDirectory = new File("${subPath}drivers/${name}-${version}-${currentPlatformName}")
+    if (!destinationDirectory.exists()) {
+        destinationDirectory.mkdirs()
+    }
+    File driverFile = new File("${destinationDirectory.absolutePath}/${driverFilePath}")
+    String localArchivePath = "${subPath}driver.${archiveFileExtension}"
 
-println("You now can use the <<browser>> object to drive your browser.")
+    if (!driverFile.exists()) {
+        def ant = new AntBuilder()
+        ant.get(src: driverDownloadFullPath, dest: localArchivePath)
+
+        switch (archiveFileExtension) {
+            case "zip":
+                ant.unzip(src: localArchivePath, dest: destinationDirectory)
+                break
+            case "tar.gz":
+                ant.untar(src: localArchivePath, dest: destinationDirectory, compression: 'gzip')
+                break
+            case "tar.bz2":
+                ant.untar(src: localArchivePath, dest: destinationDirectory, compression: 'bzip2')
+                break
+            default:
+                ant.untar(src: localArchivePath, dest: destinationDirectory)
+        }
+
+        ant.delete(file: localArchivePath)
+        ant.chmod(file: driverFile, perm: '700')
+    }
+    return driverFile
+}
+
+if (Platform.current.is(Platform.WINDOWS)) {
+    currentPlatformName = Platform.WINDOWS.name()
+    chromeDriverZipFileName = "chromedriver_win32.zip"
+    chromeDriverExecFileName = "chromedriver.exe"
+} else if (Platform.current.is(Platform.MAC)) {
+    currentPlatformName = Platform.MAC.name()
+    chromeDriverZipFileName = "chromedriver_mac64.zip"
+    chromeDriverExecFileName = "chromedriver"
+} else if (Platform.current.is(Platform.LINUX)) {
+    currentPlatformName = Platform.LINUX.name()
+    chromeDriverZipFileName = "chromedriver_linux64.zip"
+    chromeDriverExecFileName = "chromedriver"
+} else {
+    throw new RuntimeException("Unsupported operating system [${Platform.current}]")
+}
+
+chromeDriverDownloadFullPath = "https://chromedriver.storage.googleapis.com/${chromeDriverVersion}/${chromeDriverZipFileName}"
+chromeDriverLocalFile = downloadDriver(
+        currentPlatformName,
+        chromeDriverDownloadFullPath,
+        chromeDriverExecFileName,
+        'zip',
+        "chrome",
+        chromeDriverVersion)
+
+System.setProperty('webdriver.chrome.driver', chromeDriverLocalFile.absolutePath)
+
+chromeDriver = new ChromeDriver()
+
+
+browser = new Browser(driver: chromeDriver)
+
+binding = new Binding()
+updater = new BindingUpdater(binding, browser)
+
+updater.initialize()
